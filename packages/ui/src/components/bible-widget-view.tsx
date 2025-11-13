455502a7d25d5ceba283135ace2024be8a3fa9e5
cameronapak <cameronandrewpak@gmail.com>
2025-11-13 15:40:04 -0600

    refactor(ui): restructure BibleWidgetView to use grid layout and improve styling

--- packages/ui/src/components/bible-widget-view.tsx
+++ packages/ui/src/components/bible-widget-view.tsx
@@ -1,16 +1,16 @@
-import { usePassage, useVersion } from "@youversion/platform-react-hooks";
-import { BibleTextView } from "./verse";
-import { BibleAppLogoLockup } from "./bible-app-logo-lockup";
+import { usePassage, useVersion } from '@youversion/platform-react-hooks';
+import { BibleTextView } from './verse';
+import { BibleAppLogoLockup } from './bible-app-logo-lockup';
 
 type BibleWidgetViewProps = {
   reference: string;
   versionId: number;
-  background?: "light" | "dark";
+  background?: 'light' | 'dark';
 };
 export function BibleWidgetView({
   reference,
   versionId,
-  background = "light",
+  background = 'light',
 }: BibleWidgetViewProps): React.ReactNode {
   const { version } = useVersion(versionId);
   const { passage } = usePassage({
@@ -21,32 +21,33 @@ export function BibleWidgetView({
   });
 
   return (
-    <div
+    <section
       data-yv-sdk
-      data-yv-theme={background === "dark" ? "dark" : "light"}
-      className="yv:flex yv:flex-col yv: yv:mx-auto yv:w-1/2 yv:bg-background yv:p-6 yv:rounded-[8px]"
+      data-yv-theme={background === 'dark' ? 'dark' : 'light'}
+      className="yv:flex yv:flex-col yv:bg-card yv:p-6 yv:max-w-md yv:rounded-2xl"
     >
-      <section>
-        <div>
-          <h2 className="yv:font-bold yv:tracking-widest yv:text-xs yv:uppercase">
-            {passage?.human_reference} {version?.local_abbreviation}
-          </h2>
-        </div>
-      </section>
-      <section className="[&[data-yv-theme='dark']]:yv:text-foreground">
-        <BibleTextView
-          fontSize={16}
-          fontFamily={"'Source Serif Pro', serif"}
-          reference={reference}
-          versionId={versionId}
-        />
-      </section>
-      <section className="yv:flex yv:justify-between yv:items-center yv:mt-8">
-        <div className="yv:w-1/2 yv:font-bold yv:text-[9px]">
+      {passage?.human_reference ? (
+        <h2 className="yv:font-bold yv:tracking-widest yv:text-xs yv:uppercase">
+          {passage.human_reference} {version?.local_abbreviation}
+        </h2>
+      ) : null}
+
+      <BibleTextView
+        fontSize={16}
+        fontFamily={"'Source Serif Pro', serif"}
+        reference={reference}
+        versionId={versionId}
+      />
+
+      <div className="yv:grid yv:grid-cols-2 yv:items-center yv:mt-8">
+        <div className="yv:text-muted-foreground yv:justify-self-start yv:font-bold yv:text-[0.5rem]">
           {version?.copyright_short}
         </div>
-        <BibleAppLogoLockup fontSize={12} />
-      </section>
-    </div>
+
+        <div className="yv:justify-self-end">
+          <BibleAppLogoLockup fontSize={12} />
+        </div>
+      </div>
+    </section>
   );
 }
