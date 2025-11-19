524665ca231b39793c53dba4f4022fa7046decd0
Brenden Manquen <bmanquen1993@gmail.com>
2025-11-19 09:43:43 -0600

    test(core): mock all api endpoints used in tests (#30)
    * fix(core): type errors and update zod schema for bible version
    
    * test(core): mock all api endpoints being used in tests
    
    ---------
    
    Co-authored-by: Cameron Pak <cameronandrewpak@gmail.com>

--- packages/core/src/__tests__/handlers.ts
+++ packages/core/src/__tests__/handlers.ts
@@ -1,6 +1,20 @@
 import { http, HttpResponse } from 'msw';
 import type { Collection, Highlight, Language } from '../types';
 import { mockLanguages } from './MockLangauges';
+import { mockVersions, mockVersionKJV } from './MockVersions';
+import { mockBibleGenesis, mockBibleBooks } from './MockBibles';
+import { mockChapterGenesis1, mockGenesisChapters } from './MockChapters';
+import { mockGen1Verse1, mockGen1Verses } from './MockVerses';
+import {
+  mockNIVGen1PassageHTML,
+  mockNIVGen1PassageText,
+  mockNIVGen1Verse1PassageHTML,
+  mockNIVGen1Verse1PassageText,
+  mockNIVRom1PassageHTMLHeadings,
+  mockNIVRom1PassageHTMLHeadingsNotes,
+  mockNIVRom1PassageHTMLNotes,
+} from './MockPassages';
+import { mockAllVOTDs } from './MockVOTDs';
 
 const apiHost = process.env.YVP_API_HOST;
 if (!apiHost) {
@@ -89,5 +103,89 @@ export const handlers = [
 
   http.delete(`https://${apiHost}/v1/highlights/:passageId`, () => {
     return new HttpResponse(null, { status: 204 });
+  }),
+
+  // Versions endpoints
+  http.get(`https://${apiHost}/v1/bibles`, () => {
+    return HttpResponse.json({
+      data: mockVersions,
+      next_page_token: null,
+      total_size: 14,
+    });
+  }),
+
+  http.get(`https://${apiHost}/v1/bibles/:id`, () => {
+    return HttpResponse.json(mockVersionKJV);
+  }),
+
+  // Bible endpoints
+  http.get(`https://${apiHost}/v1/bibles/:id/books`, () => {
+    return HttpResponse.json({ data: mockBibleBooks });
+  }),
+
+  http.get(`https://${apiHost}/v1/bibles/:bible_id/books/:book_id`, () => {
+    return HttpResponse.json(mockBibleGenesis);
+  }),
+
+  // Chaper endpoints
+  http.get(`https://${apiHost}/v1/bibles/:bible_id/books/:book_id/chapters`, () => {
+    return HttpResponse.json({ data: mockGenesisChapters });
+  }),
+  http.get(`https://${apiHost}/v1/bibles/:bible_id/books/:book_id/chapters/:chapter_id`, () => {
+    return HttpResponse.json(mockChapterGenesis1);
+  }),
+
+  // Verse endpoints
+  http.get(
+    `https://${apiHost}/v1/bibles/:bible_id/books/:book_id/chapters/:chapter_id/verses`,
+    () => {
+      return HttpResponse.json({ data: mockGen1Verses });
+    },
+  ),
+  http.get(
+    `https://${apiHost}/v1/bibles/:bible_id/books/:book_id/chapters/:chapter_id/verses/:verse_id`,
+    () => {
+      return HttpResponse.json(mockGen1Verse1);
+    },
+  ),
+
+  // Passage endpoints
+  http.get(`https://${apiHost}/v1/bibles/:bible_id/passages/GEN.1`, ({ request }) => {
+    const url = new URL(request.url);
+    const format = url.searchParams.get('format');
+    if (format === 'html') {
+      return HttpResponse.json(mockNIVGen1PassageHTML);
+    }
+    return HttpResponse.json(mockNIVGen1PassageText);
+  }),
+  http.get(`https://${apiHost}/v1/bibles/:bible_id/passages/GEN.1.1`, ({ request }) => {
+    const url = new URL(request.url);
+    const format = url.searchParams.get('format');
+    if (format === 'html') {
+      return HttpResponse.json(mockNIVGen1Verse1PassageHTML);
+    }
+    return HttpResponse.json(mockNIVGen1Verse1PassageText);
+  }),
+  http.get(`https://${apiHost}/v1/bibles/:bible_id/passages/ROM.1`, ({ request }) => {
+    const url = new URL(request.url);
+    const includeHeadings = url.searchParams.get('include_headings') === 'true';
+    const includeNotes = url.searchParams.get('include_notes') === 'true';
+    if (includeHeadings && !includeNotes) {
+      return HttpResponse.json(mockNIVRom1PassageHTMLHeadings);
+    } else if (!includeHeadings && includeNotes) {
+      return HttpResponse.json(mockNIVRom1PassageHTMLNotes);
+    } else if (includeNotes && includeHeadings) {
+      return HttpResponse.json(mockNIVRom1PassageHTMLHeadingsNotes);
+    }
+  }),
+
+  // Verse Of The Day endpoints
+  http.get(`https://${apiHost}/v1/verse_of_the_days/:day`, ({ params }) => {
+    const { day } = params;
+    const votd = mockAllVOTDs.find((v) => v.day === Number(day));
+    return HttpResponse.json(votd || mockAllVOTDs[0]);
+  }),
+  http.get(`https://${apiHost}/v1/verse_of_the_days`, () => {
+    return HttpResponse.json({ data: mockAllVOTDs });
   }),
 ];
