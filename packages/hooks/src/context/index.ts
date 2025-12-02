268b7b75d8df8832ac95c5f63f496f6575289bbf
Brenden Manquen <bmanquen1993@gmail.com>
2025-12-01 21:48:28 -0600

    refactor(hooks,ui)!: rename BibleSDKContext/Provider and YVAuthContext/Provder
    - rename BibleSDKContext to YouVersionContext
    - rename BibleSDKProvider to YouVersionProvider
    - rename YVAuthContext to YouVersionAuthContext
    - rename YVAuthProvider to YouVersionAuthProvider
    
    BREAKING CHANGE: must use YouVersionContext instead of BibleSDKContext,
    YouVersionProvider instead of BibleSDKProvider, YouVersionAuthContext
    instead of YVAuthContext, and YouVersionAuthProvider instead of
    YVAuthProvider.

--- packages/hooks/src/context/index.ts
+++ packages/hooks/src/context/index.ts
@@ -4,5 +4,5 @@ export * from './ReaderContext';
 export * from './ReaderProvider';
 export * from './VerseSelectionProvider';
 export * from './VerseSelectionContext';
-export * from './YVAuthContext';
-export * from './YVAuthProvider';
+export * from './YouVersionAuthContext';
+export * from './YouVersionAuthProvider';
