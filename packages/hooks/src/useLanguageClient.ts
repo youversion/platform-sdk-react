d855ac20f20d445698e9035f19c7ba400491e8f5
Brenden Manquen <bmanquen1993@gmail.com>
2026-01-26 21:58:32 -0600

    feat(hooks): Add get language endpoint
    - add endpoint to retrieve a specific language data
    - abstract the language client logic into its own hook
    - create tests for get language endpoint
    - move tests for language client from useLanguages tests to
    its own file

---
+++ packages/hooks/src/useLanguageClient.ts
@@ -0,0 +1,25 @@
+'use client';
+
+import { useContext, useMemo } from 'react';
+import { YouVersionContext } from './context';
+import { ApiClient, LanguagesClient } from '@youversion/platform-core';
+
+export function useLanguagesClient(): LanguagesClient {
+  const context = useContext(YouVersionContext);
+
+  return useMemo(() => {
+    if (!context?.appKey) {
+      throw new Error(
+        'YouVersion context not found. Make sure your component is wrapped with YouVersionProvider and an API key is provided.',
+      );
+    }
+
+    return new LanguagesClient(
+      new ApiClient({
+        appKey: context.appKey,
+        apiHost: context.apiHost,
+        installationId: context.installationId,
+      }),
+    );
+  }, [context?.apiHost, context?.appKey, context?.installationId]);
+}
