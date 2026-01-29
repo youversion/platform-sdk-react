a8d192d63e05a78a92ab2f279ce5f80ddb080d95
Cameron Pak <cameronandrewpak@gmail.com>
2026-01-29 15:57:30 -0600

    Chore: Remove duplicate cleanup in vitest setup

--- packages/ui/.storybook/vitest.setup.ts
+++ packages/ui/.storybook/vitest.setup.ts
@@ -1,15 +1,10 @@
 import { setProjectAnnotations } from '@storybook/react-vite';
-import { afterEach, beforeEach } from 'vitest';
+import { beforeEach } from 'vitest';
 import * as previewAnnotations from './preview';
 
 const _annotations = setProjectAnnotations([previewAnnotations]);
 
 beforeEach(() => {
-  localStorage?.clear?.();
-  sessionStorage?.clear?.();
-});
-
-afterEach(() => {
   localStorage?.clear?.();
   sessionStorage?.clear?.();
 });
