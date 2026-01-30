d73bceb830048a44217f028c7790502ba745909f
Jared Hightower <jared.hightower.contractor@youversion.com>
2026-01-30 12:48:26 -0600

    chore: removed ReaderProviderOptions type

--- packages/hooks/src/types/bibles.ts
+++
@@ -1,8 +0,0 @@
-import type { BibleBook, BibleChapter, BibleVerse, BibleVersion } from '@youversion/platform-core';
-
-export interface ReaderProviderOptions {
-  currentVersion: BibleVersion;
-  currentBook: BibleBook;
-  currentChapter: BibleChapter;
-  currentVerse: BibleVerse | null;
-}
