c8c3abba2c26b0637ea2b3d3786bfc7fa695dbf9
Cameron Pak <cameronandrewpak@gmail.com>
2026-03-09 12:21:38 -0500

    Updated example app to showcase BOTH of the VOTD card component sizes

--- examples/vite-react/src/pages/VotdPage.tsx
+++ examples/vite-react/src/pages/VotdPage.tsx
@@ -2,8 +2,9 @@ import { VerseOfTheDay } from '@youversion/platform-react-ui';
 
 export function VotdPage() {
   return (
-    <div className="flex justify-center p-6 md:p-12">
-      <VerseOfTheDay versionId={3034} />
+    <div className="flex flex-col grow items-center gap-8 w-full p-6 md:p-12">
+      <VerseOfTheDay size="default" versionId={3034} />
+      <VerseOfTheDay size="lg" versionId={3034} />
     </div>
   );
 }
