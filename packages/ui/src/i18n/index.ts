3642b5f15cafb08fb4d8d4f791159eb9eabfd0f7
camrun01 <cameron.b.llewellyn@gmail.com>
2026-04-08 11:06:19 -0500

    chore: implement i18next

---
+++ packages/ui/src/i18n/index.ts
@@ -0,0 +1,23 @@
+import i18next, { type i18n as I18nInstance } from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import en from './locales/en.json';
+
+const defaultNS = 'translation';
+
+export const resources = {
+  en: { [defaultNS]: en },
+} as const;
+
+const i18n: I18nInstance = i18next.createInstance();
+
+void i18n.use(initReactI18next).init({
+  resources,
+  lng: 'en',
+  defaultNS,
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false, // React already escapes
+  },
+});
+
+export default i18n;
