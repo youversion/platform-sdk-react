66be747f51389ca6ff9459336805e4727a0db084
camrun01 <cameron.b.llewellyn@gmail.com>
2026-06-03 16:05:41 -0500

    chore: add language detection

--- packages/ui/src/components/YouVersionProvider.tsx
+++ packages/ui/src/components/YouVersionProvider.tsx
@@ -1,10 +1,15 @@
-import React, { type ComponentProps } from 'react';
+import React, { type ComponentProps, useLayoutEffect } from 'react';
 import { YouVersionProvider as BaseYouVersionProvider } from '@youversion/platform-react-hooks';
+import { syncBrowserLanguageFromNavigator } from '@/i18n';
 import { YvStyles } from '@/lib/yv-styles';
 
 export function YouVersionProvider(
   props: ComponentProps<typeof BaseYouVersionProvider>,
 ): React.ReactElement {
+  useLayoutEffect(() => {
+    syncBrowserLanguageFromNavigator();
+  }, []);
+
   return (
     <BaseYouVersionProvider {...props}>
       <YvStyles />
