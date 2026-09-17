ce97bde9acee3373615766df0f5abd3e2b4c9fc0
Austin <abharms@gmail.com>
2026-09-17 11:12:09 -0500

    test(ui): mock font stylesheet in Storybook

--- packages/ui/src/test/mocks/handlers.ts
+++ packages/ui/src/test/mocks/handlers.ts
@@ -13,6 +13,10 @@ function pathParam(value: string | readonly string[] | undefined): string | unde
 }
 
 export const globalHandlers = [
+  // Font stylesheet loaded by YouVersionProvider
+  http.get('*/v1/fonts/1/stylesheet', () =>
+    HttpResponse.text('', { headers: { 'Content-Type': 'text/css' } }),
+  ),
   // Organization (publisher) lookup for the version picker
   http.get('*/v1/organizations/:id', ({ params }) => {
     const id = pathParam(params.id);
